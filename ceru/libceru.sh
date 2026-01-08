33effeffa8c2e269568aaffe22b102a9ac62e745
Emile Clark-Boman <emile@agribit.com.au>
2026-01-08 14:03:59 +1000

    add missing Apache2 statement to ceru

--- ceru/libceru.sh
+++ ceru/libceru.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
+# Copyright 2025 Emile Clark-Boman
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # libceru relies on the script that sources
 # it to set the following environment variables:
 #     CMD_NAME USAGE
-set -u 
+
+set -u
 
 # ======== INTERNAL STATE ========
 SUBCMDS="$PWD/subcmds" # XXX: TODO: don't hardcode as relative
