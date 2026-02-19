56184d62fb66e060960ee8d403c829d5225f5019
_cry64 <them@dobutterfliescry.net>
2026-02-19 14:38:52 +1000

    clarify license

--- cerulean/nexus/nexus.nix
+++ cerulean/nexus/nexus.nix
@@ -1,4 +1,4 @@
-# Copyright 2025 Emile Clark-Boman
+# Copyright 2025-2026 _cry64 (Emile Clark-Boman)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
